06fca6053fe088726dd5ef159f01ce04adc63dd8
daniela <gaofulou05@gmail.com>
2023-10-20 00:00:53 +0200

    Versió2.2

--- practica.sh
+++ practica.sh
@@ -47,40 +47,6 @@ while [[ $opcio!=q ]]; do #bucle
 		'lcp')
 			cut -d',' -f2,7,11 cities.csv | grep -w $codiP | cut -d',' -f1,3 | column -s',' -t
 			;;
-
-		'ecp')
-			#Aquí està pensat perquè cada cop que es canvii de país, la llista substitueixi les dades anteriors per les noves i només hi hagi 1 arxiu
-			cut -d',' -f2,7,11 cities.csv | grep -w $codiP | cut -d',' -f1,3 > PoblacionsPais.csv
-			mv PoblacionsPais.csv $codiP.csv 
-			;;
-		'lce')
-			cut -d',' -f2,4,7,11 cities.csv | grep -w $codiP | grep -w $codiE | cut -d',' -f1,4 | column -s',' -t
-			;;
-		
-		'ece')
-			cut -d',' -f2,4,7,11 cities.csv | grep -w $codiP | grep -w $codiE | cut -d',' -f1,4 > PoblacionsEstat.csv
-			mv PoblacionsEstat.csv ${codiP}_${codiE}.csv #Igual que l'ordre ecp respecte la quantitat d'arxius
-			;;
-		'gwd')
-			read -p "Nom d'una població: " poblacio
-			wikiData=$(cut -d',' -f2,4,7,11 cities.csv | grep -w $codiP | grep -w $codiE | grep -w ^$poblacio | cut -d',' -f4)
-			if [[ -n $wikiData ]]
-			then
-				curl https://www.wikidata.org/wiki/Special:EntityData/$widiData.json > wiki.json
-				mv wiki.json $wikiData.json #Igual que l'ordre ecp i ece respecta la quantitat d'arxius
-			fi
-			;;
-		'est') 
-			awk -F',' 'BEGIN { nord=0.0; sud=0.0; est=0.0; oest=0.0; nu=0.0; wiki=0.0 } 
-			{ if (NR > 0) {  
-				nord += ( $9 > 0.0); 
-				sud += ( $9 < 0.0 ); 
-				est += ($10 > 0.0); 
-				oest += ($10 < 0.0); 
-				wiki += ($11 == ""); 
-				nu += ($10 == 0) && ($9 == 0) }
-		       	} END { print "Nord", nord, "Sud", sud, "Est", est, "Oest", oest, "No ubicació", nu, "No WDId", wiki}' cities.csv
-			;; #NR és una variable interna de awk que determina el nombre de la línia
 		*)#Qualsevol cosa que no sigui una des les opcions
 			echo "Sense argument vàlid"
 			;;			
